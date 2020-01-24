55bb270baad8be509db55ba7741caacceeb1eafe
Chris Troutner <chris.troutner@gmail.com>
2020-01-23 17:49:26 -0800

    Added IPFS upload

--- index.js
+++ index.js
@@ -5,6 +5,9 @@
 const WebTorrent = require('webtorrent')
 const client = new WebTorrent()
 
+const IPFS = require('./lib/ipfs')
+const ipfs = new IPFS()
+
 const fileName = `./fox.jpg`
 
 const options = {
@@ -13,15 +16,18 @@ const options = {
 
 async function runTest() {
   try {
+    // Report any torrent errors.
     client.on('error', err => {
       console.error(`Error with webtorrent: `, err)
     })
 
-    // client.seed(fileName, options, torrent => {
-    //   // console.log(`File is being seeded: ${JSON.stringify(torrent.info,null,2)}`)
-    //   console.log(`File is being seeded. Magnet URI: ${torrent.magnetURI}`)
-    //   // console.log(`File is being seeded: `, torrent)
-    // })
+    // start IPFS
+    await ipfs.startIPFS()
+
+    // Upload file to IPFS
+    const ipfsData = await ipfs.upload(fileName)
+    const ipfsHash = ipfsData[ipfsData.length - 1].hash
+    console.log(`IPFS hash: ${ipfsHash}`)
 
     // const torrent = await client.seed(fileName, options)
     const magnetURI = await seedFile(fileName)
