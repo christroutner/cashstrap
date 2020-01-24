be91c9a65eef5d62ef6a4c644c28aa287d87f083
Chris Troutner <chris.troutner@gmail.com>
2020-01-23 17:59:19 -0800

    Customized for bootstrap server

--- index.js
+++ index.js
@@ -2,52 +2,74 @@
   An app that syndicates files over both bittorrent and IPFS.
 */
 
-const WebTorrent = require('webtorrent')
-const client = new WebTorrent()
+const WebTorrent = require("webtorrent");
+const client = new WebTorrent();
 
-const IPFS = require('./lib/ipfs')
-const ipfs = new IPFS()
+const IPFS = require("./lib/ipfs");
+const ipfs = new IPFS();
 
-const fileName = `./fox.jpg`
+const fileNames = [
+  `slpdb-mainnet-01-14-2020-929f59a5550cf269bc49a2c7853fd0bf59ec894a-617806.zip`,
+  `blockbook-testnet-01-21-2020-1355060.zip`,
+  `blockbook-mainnet-01-21-20-618876.zip`,
+  `bitcore-node-testnet-09-13-2019.zip`,
+  `bitcore-node-bch-mainnet-618830-01-21-2020.zip`
+];
+
+const filePath = "../";
+
+// const fileName = `./fox.jpg`
 
 const options = {
-  name: 'fox.jpg'
-}
+  name: "fox.jpg"
+};
 
-async function runTest() {
+async function uploadFiles() {
   try {
     // Report any torrent errors.
-    client.on('error', err => {
-      console.error(`Error with webtorrent: `, err)
-    })
+    client.on("error", err => {
+      console.error(`Error with webtorrent: `, err);
+    });
 
     // start IPFS
-    await ipfs.startIPFS()
+    await ipfs.startIPFS();
 
-    // Upload file to IPFS
-    const ipfsData = await ipfs.upload(fileName)
-    const ipfsHash = ipfsData[ipfsData.length - 1].hash
-    console.log(`IPFS hash: ${ipfsHash}`)
+    // Loop through each file.
+    for (let i = 0; i < fileNames.length; i++) {
+      const fileName = fileNames[i];
 
-    // const torrent = await client.seed(fileName, options)
-    const magnetURI = await seedFile(fileName)
-    console.log(`File is being seeded. Magnet URI: ${magnetURI}`)
-  } catch(err) {
-    console.error(`Error in runTest: `, err)
+      console.log(`Preparing to share this file: `);
+      console.log(fileName);
+
+      // Upload file to IPFS
+      const ipfsData = await ipfs.upload(fileName);
+      const ipfsHash = ipfsData[ipfsData.length - 1].hash;
+      console.log(`IPFS hash: ${ipfsHash}`);
+
+      // const torrent = await client.seed(fileName, options)
+      const magnetURI = await seedFile(fileName);
+
+      console.log(`File is being seeded as torrent. Magnet URI:`);
+      console.log(magnetURI);
+
+      console.log(" ");
+    }
+  } catch (err) {
+    console.error(`Error in runTest: `, err);
   }
 }
-runTest()
+uploadFiles();
 
 // Wraps the seed call in a promise.
 function seedFile(filename) {
   try {
     return new Promise(resolve => {
       client.seed(filename, options, torrent => {
-        return resolve(torrent.magnetURI)
-      })
+        return resolve(torrent.magnetURI);
+      });
     });
-  } catch(err) {
-    console.error(`Error in seedFile()`)
-    throw err
+  } catch (err) {
+    console.error(`Error in seedFile()`);
+    throw err;
   }
 }
