fd95fd2f80dbeb5c17d2fa80316745e285f95f80
Chris Troutner <chris.troutner@gmail.com>
2020-10-30 07:40:37 -0700

    Fixing bug

--- serve-cashstrap.js
+++ serve-cashstrap.js
@@ -9,15 +9,42 @@ const client = new WebTorrent();
 // const ipfs = new IPFS();
 
 const fileNames = [
-  `/mnt/HC_Volume_4733554/files/abc-mainnet-658958-v0.22.4-10-27-2020.zip`,
-  `/mnt/HC_Volume_4733554/files/abc-testnet-1417941-v0.22.5-10-27-2020.zip`,
-  `/mnt/HC_Volume_4733554/files/blockbook-bch-mainnet-v0.3.2-10-28-2020.zip`,
-  `/mnt/HC_Volume_4733554/files/blockbook-bch-testnet-v0.3.2-10-28-2020.zip`,
-  `/mnt/HC_Volume_4733554/files/slpdb-mainnet-08-06-2020-647202-2f23af5122f11c24fb86026bea2b198a024c8f9b.zip`,
-  `/mnt/HC_Volume_4733554/files/slpdb-testnet-08-06-2020-1399300-2f23af5122f11c24fb86026bea2b198a024c8f9b.zip`,
-  `/mnt/HC_Volume_4733554/files/fulcrum-mainnet-10-28-2020.zip`,
-  `/mnt/HC_Volume_4733554/files/fulcrum-testnet-10-29-2020.zip`,
-  '/mnt/HC_Volume_7319195/bchn-mainnet-655089-09-30-2020.zip'
+  {
+    path: '/mnt/HC_Volume_4733554/files/',
+    filename: 'abc-mainnet-658958-v0.22.4-10-27-2020.zip'
+  },
+  {
+    path: '/mnt/HC_Volume_4733554/files/',
+    filename: 'abc-testnet-1417941-v0.22.5-10-27-2020.zip'
+  },
+  {
+    path: '/mnt/HC_Volume_4733554/files/',
+    filename: 'blockbook-bch-mainnet-v0.3.2-10-28-2020.zip'
+  },
+  {
+    path: '/mnt/HC_Volume_4733554/files/',
+    filename: 'blockbook-bch-testnet-v0.3.2-10-28-2020.zip'
+  },
+  {
+    path: '/mnt/HC_Volume_4733554/files/',
+    filename: 'slpdb-mainnet-08-06-2020-647202-2f23af5122f11c24fb86026bea2b198a024c8f9b.zip'
+  },
+  {
+    path: '/mnt/HC_Volume_4733554/files/',
+    filename: 'slpdb-testnet-08-06-2020-1399300-2f23af5122f11c24fb86026bea2b198a024c8f9b.zip'
+  },
+  {
+    path: '/mnt/HC_Volume_4733554/files/',
+    filename: 'fulcrum-mainnet-10-28-2020.zip'
+  },
+  {
+    path: '/mnt/HC_Volume_4733554/files/',
+    filename: 'fulcrum-testnet-10-29-2020.zip'
+  },
+  {
+    path: '/mnt/HC_Volume_4733554/files/',
+    filename: 'bchn-mainnet-655089-09-30-2020.zip'
+  }
 ];
 
 // const filePath = "/mnt/HC_Volume_4733554/files/";
@@ -36,12 +63,11 @@ async function uploadFiles() {
 
     // Loop through each file.
     for (let i = 0; i < fileNames.length; i++) {
-      const fileName = fileNames[i];
+      const fileName = fileNames[i].filename;
+      const path = `${fileNames[i].path}${fileName}`;
 
       console.log(`Preparing to share this file: `);
       console.log(fileName);
-
-      const path = `${fileName}`;
 
       // Upload file to IPFS
       // const ipfsData = await ipfs.upload(path);
